3a2f2eaca65a12674ec6892662b3533d9ba242c2
Caio Deberaldini <caio-soad@hotmail.com>
2024-10-03 20:32:39 -0400

    JAX class update

--- models.py
+++ models.py
@@ -8,7 +8,28 @@ class CNNEmulator(eqx.Module):
     layers: list
 
     def __init__(self, key: PRNGKeyArray, hidden_dim: Int = 4):
-        raise NotImplementedError
+        self.layers = eqx.nn.Sequential([
+            eqx.nn.Conv2d(2, hidden_dim, 3, 1, 1, key=key),
+            eqx.nn.Lambda(jnp.tanh),
+            eqx.nn.Conv2d(hidden_dim, hidden_dim, 3, 1, 1, key=key),
+            eqx.nn.Lambda(jnp.tanh),
+            eqx.nn.Conv2d(hidden_dim, hidden_dim, 3, 1, 1, key=key),
+            eqx.nn.Lambda(jnp.tanh),
+            eqx.nn.Conv2d(hidden_dim, hidden_dim, 3, 1, 1, key=key),
+            eqx.nn.Lambda(jnp.tanh),
+            eqx.nn.Conv2d(hidden_dim, 1, 3, 1, 1, key=key)
+        ])
 
     def __call__(self, x: Float[Array, "2 n_res n_res"]) -> Float[Array, "1 n_res n_res"]:
-        raise NotImplementedError
+        out = x
+        for layer in self.layers:
+            out = layer(out)
+
+        return out
+
+    def rollout(self, x: Float[Array, "2 n_res n_res"], n_step: Int) -> Float[Array, "n_step n_res n_res"]:
+        result = [x]
+        for i in range(n_step):
+            x = jnp.concatenate([x[1:], self(x)], axis=0)
+            result.append(x[1:])
+        return jnp.stack(result)
